5f198e1f163094232165f7f138868519c5b111ef
krityk <43952333+krityk@users.noreply.github.com>
2019-04-10 15:32:15 +0200

    Create Contact.js

---
+++ src/sections/Contact.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Box, Image, Flex } from 'rebass';
+import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import Fade from 'react-reveal/Fade';
+import Section from '../components/Section';
+import Triangle from '../components/Triangle';
+import markdownRenderer from '../components/MarkdownRenderer';
+
+const Background = () => (
+  <div>
+    <Triangle
+      color="backgroundDark"
+      height={['15vh', '10vh']}
+      width={['100vw', '100vw']}
+      invertX
+    />
+
+    <Triangle
+      color="secondary"
+      height={['50vh', '40vh']}
+      width={['70vw', '40vw']}
+      invertY
+    />
+
+    <Triangle
+      color="primaryDark"
+      height={['40vh', '15vh']}
+      width={['100vw', '100vw']}
+      invertX
+      invertY
+    />
+  </div>
+);
+
+const Contact = () => (
+  <Section.Container id="Me contacter" Background={Background}>
+    <Section.Header name="Me contacter" />
+
+    <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
+      <Fade bottom>
+        <div>
+          <h3> Vous avez des questions, un projet ?  </h3>
+
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Nom: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Adresse Mail: <input type="email" name="email" />
+              </label>
+            </p>
+
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
+      </Fade>
+
+      <Fade right />
+    </Flex>
+  </Section.Container>
+);
+
+export default Contact;
