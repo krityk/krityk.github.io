fd4ffb7b750a6f770fd193870a31b2dadd134cca
krityk <43952333+krityk@users.noreply.github.com>
2019-04-12 16:04:08 +0200

    Create Form.js

---
+++ src/sections/Form.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Box, Image, Flex, Button } from 'rebass';
+import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import Fade from 'react-reveal/Fade';
+import Section from '../components/Section';
+import Triangle from '../components/Triangle';
+import markdownRenderer from '../components/MarkdownRenderer';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const Background = () => (
+  <div>
+    <Triangle
+      color="backgroundDark"
+      height={['15vh', '10vh']}
+      width={['100vw', '100vw']}
+      invertX
+    />
+
+    <Triangle
+      color="secondary"
+      height={['50vh', '40vh']}
+      width={['70vw', '40vw']}
+      invertY
+    />
+
+    <Triangle
+      color="primaryDark"
+      height={['40vh', '15vh']}
+      width={['100vw', '100vw']}
+      invertX
+      invertY
+    />
+  </div>
+);
+
+const Contact = () => (
+  <Section.Container id="Me contacter" Background={Background}>
+    <Section.Header name="Me contacter" />
+
+    <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
+      <Fade bottom>
+        <div>
+          <h3> Vous avez des questions, un projet ?
+            <FontAwesome name={faHome} />  </h3>
+          <div>
+
+  </div>
+
+          <form name="contact" method="POST" data-netlify="true" action="../src/sections/Contact.js" >
+          <Flex>
+          <Box
+    p={3}
+    width={1/2}
+    color='magenta'
+    bg='black'>
+    <p>Nom:</p>
+    <p>Email:</p>
+    <p>Message:</p>
+    </Box>
+    <Box
+    p={3}
+    width={1/2}
+    color='magenta'
+    bg='black'>
+    <input type="text" name="name" />
+    <input type="email" name="email" />
+    <textarea name="message" />
+    </Box>
+    </Flex>
+
+
+              <Button variant='primary' type="submit">Send</Button>
+
+          </form>
+        </div>
+      </Fade>
+
+      <Fade right />
+    </Flex>
+  </Section.Container>
+);
+
+export default Contact;
